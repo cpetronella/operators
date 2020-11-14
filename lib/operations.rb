8712346e77fab73716e0edb194615f2d04741e94
Christopher <christopher.petronella@gmail.com>
2020-11-14 11:03:02 -0500

    commit tree df73294b4f9dd40cf29ad33233b47975e6e5c152

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,29 @@
-def unsafe?(speed)
 
-end
+#def unsafe(speed)
+#if speed > 60 : true
+#end
 
 
 
-def not_safe?(speed)
+#def not_safe?(speed)
 	
-end
+#end
 	
+def unsafe?(speed) 
+if speed > 60
+    true
+elsif 
+    speed < 40
+    true
+  else
+    false
+   end
+  end
+
+  def not_safe?(speed)
+    speed < 40 || speed > 60  ? true : false
+    end
 
 
+    
+    
